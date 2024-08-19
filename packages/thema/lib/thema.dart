710080d3e11a297e56e806256fd53f23549f669d
ronnnnn <xxxx.xyzs.xxxx@gmail.com>
2024-08-19 18:36:41 +0900

    refactor: refactor

--- packages/thema/lib/thema.dart
+++ packages/thema/lib/thema.dart
@@ -1,11 +1,12 @@
 import 'dart:async';
 
+import 'package:collection/collection.dart';
 import 'package:macros/macros.dart';
 
 final _flutterMaterial = Uri.parse('package:flutter/material.dart');
 final _dartCore = Uri.parse('dart:core');
 
-macro class Thema implements ClassTypesMacro, ClassDeclarationsMacro {
+macro class Thema implements ClassTypesMacro, ClassDeclarationsMacro, ClassDefinitionMacro {
   const Thema();
 
   @override
@@ -20,11 +21,30 @@ macro class Thema implements ClassTypesMacro, ClassDeclarationsMacro {
 
     builder.extendsType(superTypeCode);
   }
-  
+
   @override
   FutureOr<void> buildDeclarationsForClass(ClassDeclaration clazz, MemberDeclarationBuilder builder) async {
-    final fields = await builder.fieldsOf(clazz);
+    await _declareConstractor(clazz: clazz, builder: builder);
 
+    await (
+      _declareCopyWith(clazz: clazz, builder: builder),
+      _declareLeap(clazz: clazz, builder: builder),
+    ).wait;
+  }
+
+  @override
+  FutureOr<void> buildDefinitionForClass(ClassDeclaration clazz, TypeDefinitionBuilder builder) async {
+    await (
+      _defineCopyWith(clazz: clazz, typeDefinisionBuilder: builder),
+      _defineLeap(clazz: clazz, typeDefinisionBuilder: builder),
+    ).wait;
+  }
+
+  Future<void> _declareConstractor({
+    required ClassDeclaration clazz,
+    required MemberDeclarationBuilder builder,
+  }) async {
+    final fields = await builder.fieldsOf(clazz);
     final constructorParameterCodes = fields.map((field) =>
      ParameterCode(
       keywords: ['required'],
@@ -40,9 +60,14 @@ macro class Thema implements ClassTypesMacro, ClassDeclarationsMacro {
       ],
     );
     builder.declareInType(constructorCode);
+  }
 
+  Future<void> _declareCopyWith({
+    required ClassDeclaration clazz,
+    required MemberDeclarationBuilder builder,
+  }) async {
+    final fields = await builder.fieldsOf(clazz);
     final themeExtensionIdentifier = await builder.resolveIdentifier(_flutterMaterial, 'ThemeExtension');
-
     final copyWithReturnTypeCode = NamedTypeAnnotationCode(
         name: themeExtensionIdentifier,
         typeArguments: [
@@ -55,31 +80,22 @@ macro class Thema implements ClassTypesMacro, ClassDeclarationsMacro {
       name: field.identifier.name,
     ),
     ).toList();
-    final instiatePareterCodes = fields.map((field) {
-      final fieldName = field.identifier.name;
-      return DeclarationCode.fromString(
-        '$fieldName: $fieldName ?? this.$fieldName',
-      );
-    },
-    ).toList();
-    final copyWithFunctionBodyCode = FunctionBodyCode.fromParts([
-        '    return $constructorName(\n',
-        ...instiatePareterCodes.joinAsCode(',\n'),
-        '    );\n',
-    ]);
     final copyWithFunctionCode = DeclarationCode.fromParts(
       [
         '  ',
         copyWithReturnTypeCode,
         ' copyWith({\n',
         ...copyWithParameterCodes.joinAsCode(',\n'),
-        '  }) {\n',
-        copyWithFunctionBodyCode,
-        '  }',
+        '  });',
       ],
     );
     builder.declareInType(copyWithFunctionCode);
+  }
 
+  Future<void> _declareLeap({
+    required ClassDeclaration clazz,
+    required MemberDeclarationBuilder builder,
+  }) async {
     final leapReturnTypeCode = NamedTypeAnnotationCode(name: clazz.identifier);
     final doubleIdentifier = await builder.resolveIdentifier(_dartCore, 'double');
     final leapFirstParameterCode = ParameterCode(
@@ -95,7 +111,71 @@ macro class Thema implements ClassTypesMacro, ClassDeclarationsMacro {
       leapFirstParameterCode,
       leapSecondParameterCode,
     ].toList();
+    final leapFunctionCode = DeclarationCode.fromParts(
+      [
+        '  ',
+        leapReturnTypeCode,
+        ' lerp(',
+        ...leapParameterCodes.joinAsCode(',\n'),
+        ');',
+      ],
+    );
+    builder.declareInType(leapFunctionCode);
+  }
+
+  Future<void> _defineCopyWith({
+    required ClassDeclaration clazz,
+    required TypeDefinitionBuilder typeDefinisionBuilder,
+  }) async {
+    final methods = await typeDefinisionBuilder.methodsOf(clazz);
+    final copyWithDeclaration = methods.firstWhereOrNull((method) => method.identifier.name == 'copyWith');
+    if (copyWithDeclaration == null) return;
+
+    final fields = await typeDefinisionBuilder.fieldsOf(clazz);
+    final instiatePareterCodes = fields.map((field) {
+      final fieldName = field.identifier.name;
+      return DeclarationCode.fromString(
+        '$fieldName: $fieldName ?? this.$fieldName',
+      );
+    },
+    ).toList();
+    final constructorName = clazz.identifier.name;
+    final copyWithFunctionBodyCode = FunctionBodyCode.fromParts([
+        '  {\n',
+        '    return $constructorName(\n',
+        ...instiatePareterCodes.joinAsCode(',\n'),
+        '    );\n',
+        '  }',
+    ]);
+
+    final builder = await typeDefinisionBuilder.buildMethod(copyWithDeclaration.identifier);
+    builder.augment(copyWithFunctionBodyCode);
+  }
+
+  Future<void> _defineLeap({
+    required ClassDeclaration clazz,
+    required TypeDefinitionBuilder typeDefinisionBuilder,
+  }) async {
+    final methods = await typeDefinisionBuilder.methodsOf(clazz);
+    final leapDeclaration = methods.firstWhereOrNull((method) => method.identifier.name == 'lerp');
+    if (leapDeclaration == null) return;
+
+    final leapReturnTypeCode = NamedTypeAnnotationCode(name: clazz.identifier);
+    final leapFirstParameterCode = ParameterCode(
+      keywords: ['covariant'],
+      type: NullableTypeAnnotationCode(leapReturnTypeCode),
+      name: 'other',
+    );
+    final themeExtensionIdentifier = await typeDefinisionBuilder.resolveIdentifier(_flutterMaterial, 'ThemeExtension');
+    final doubleIdentifier = await typeDefinisionBuilder.resolveIdentifier(_dartCore, 'double');
+    final leapSecondParameterCode = ParameterCode(
+      type: NamedTypeAnnotationCode(name: doubleIdentifier),
+      name: 't',
+    );
+    final constructorName = clazz.identifier.name;
+    final fields = await typeDefinisionBuilder.fieldsOf(clazz);
     final leapFunctionBodyCode = FunctionBodyCode.fromParts([
+        '  {\n',
         '    if (${leapFirstParameterCode.name} == null) {\n',
         '      return this;\n',
         '    }\n',
@@ -104,8 +184,8 @@ macro class Thema implements ClassTypesMacro, ClassDeclarationsMacro {
           final fieldName = field.identifier.name;
           final fieldTypeCode = field.type.code;
 
-          final fieldStaticType = await builder.resolve(fieldTypeCode);
-          final themeExtensionStaticType = await builder.resolve(NamedTypeAnnotationCode(name: themeExtensionIdentifier));
+          final fieldStaticType = await typeDefinisionBuilder.resolve(fieldTypeCode);
+          final themeExtensionStaticType = await typeDefinisionBuilder.resolve(NamedTypeAnnotationCode(name: themeExtensionIdentifier));
           if (await fieldStaticType.isSubtypeOf(themeExtensionStaticType)) {
             return DeclarationCode.fromParts(
               [
@@ -124,19 +204,10 @@ macro class Thema implements ClassTypesMacro, ClassDeclarationsMacro {
           );
         }).toList())).joinAsCode(',\n'),
         '    );\n',
-    ]);
-    final leapFunctionCode = DeclarationCode.fromParts(
-      [
-        '  ',
-        leapReturnTypeCode,
-        ' lerp(',
-        ...leapParameterCodes.joinAsCode(',\n'),
-        ') {\n',
-        leapFunctionBodyCode,
         '  }',
-      ],
-    );
+    ]);
 
-    builder.declareInType(leapFunctionCode);
+    final builder = await typeDefinisionBuilder.buildMethod(leapDeclaration.identifier);
+    builder.augment(leapFunctionBodyCode);
   }
 }
